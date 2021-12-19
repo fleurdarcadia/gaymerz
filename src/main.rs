b75643fa302f477a1d58e8b0a50bbc496c9a28f2
Arcadia <arcadia.liz.rose@gmail.com>
2021-12-18 20:27:16 -0500

    Create a 32x32 grid on which we can draw things

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
+use std::cell::RefCell;
+
 use ggez::event::{KeyCode, KeyMods};
 use ggez::{event, graphics, timer, Context, GameResult};
 use ggez::event::EventHandler;
 
 
-struct GameState;
+const GRID_WIDTH: usize = 32;
+const GRID_HEIGHT: usize = 32;
+
+#[derive(Copy, Clone)]
+enum Tile {
+    Empty,
+    Ground,
+}
+
+struct GameState {
+    grid: RefCell<[[Tile; GRID_WIDTH]; GRID_HEIGHT]>,
+}
+
+impl GameState {
+    fn new() -> Self {
+        GameState {
+            grid: RefCell::new([[Tile::Empty; GRID_WIDTH]; GRID_HEIGHT]),
+        }
+    }
+
+    fn set_tile(&self, x: usize, y: usize, tile: Tile) -> Result<(), &str> {
+        if x >= GRID_WIDTH || y >= GRID_HEIGHT {
+            return Err("Tile position out of bounds!");
+        }
+
+        self.grid.borrow_mut()[y][x] = tile;
+
+        Ok(())
+    }
+
+    fn create_flat_ground(&self) -> Result<(), &str> {
+        for i in 0..GRID_WIDTH {
+            self.set_tile(i, GRID_HEIGHT, Tile::Ground)?;
+        }
+
+        Ok(())
+    }
+}
 
 impl EventHandler<ggez::GameError> for GameState{
     fn update(&mut self, _ctx: &mut Context) -> GameResult{
@@ -13,7 +52,6 @@ impl EventHandler<ggez::GameError> for GameState{
     fn draw(&mut self, _ctx: &mut Context) -> GameResult{
         Ok(())
     }
-    
 }
 
 fn main() -> GameResult {
@@ -22,6 +60,8 @@ fn main() -> GameResult {
         .window_mode(ggez::conf::WindowMode::default().dimensions(800.0,600.0))
         .build()?;
     
-    let state = GameState;
+    let mut state = GameState::new();
+    state.create_flat_ground();
+
     event::run(ctx,events_loop, state)
 }
